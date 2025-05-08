432fa8a8edd9b18a4246ebdd2589de0bb65a706d
Yuta0413 <s2422016@stu.musashino-u.ac.jp>
2025-05-08 09:56:24 +0900

    APIデータをjson形式で出力するようにURLとコードをを編集

--- OC/narou_API.js
+++ OC/narou_API.js
@@ -1,9 +1,14 @@
 import fetch from 'node-fetch';
+import fs from 'fs';
 
 async function callApi() {
-    const res = await fetch("https://api.syosetu.com/novelapi/api/?of=t-w-s&lim=200");
-    const text = await res.text();
-    console.log(text);
+    const res = await fetch("https://api.syosetu.com/novelapi/api/?out=json&of=t-w-s&lim=200");
+    const json = await res.json();
+
+    const novels = json.slice(1);
+
+    fs.writeFileSync('OC/novel_info.json', JSON.stringify(novels, null, 2));
+    console.log(`${novels.length} 件の小説データを保存しました`);
 }
 
 callApi();
