896004c2fe7d301b20910835c2abc3cbb18fc6db
Yuta0413 <s2422016@stu.musashino-u.ac.jp>
2025-05-14 16:54:16 +0900

    内容を少し編集

--- My_Portfolio/src/App.js
+++ My_Portfolio/src/App.js
@@ -9,7 +9,6 @@ return (
             <p><a href="#About">About Me</a></p>
             <p><a href="#qualifications">Qualifications</a></p>
             <p><a href="#language">Programming language</a></p>
-            <p><a href="#research">Research</a></p>
             <p><a href="#sns">SNS</a></p>
     </header>
 
@@ -51,18 +50,6 @@ return (
         </nav>
         </section>
 
-        <section id="research">
-        <h2>現在の研究</h2>
-        <nav>
-            <ul>
-            <p>研究タイトル:時系列データと仕事率を用いたパーソナル管理システムの実現</p>
-            <p>研究の概要:<a href="https://docs.google.com/presentation/d/1yz86yZaYNgM084oPdGhHmK8QBR4xkimi5nGWIHKdaDI/edit?usp=sharing" target="_blank">Googleスライド</a></p>
-            <p>研究のゴール:時空間データと仕事率を用いた仕事、健康管理を行い個々の作業パフォーマンス向上と健康維持の両立を図ること</p>
-            <p>1年次の研究成果(PDFファイル):<a href="pdf.html" target="_blank">1年次成果発表会ポスター</a></p>
-            </ul>
-        </nav>
-        </section>
-
         <section id="sns">
             <h2></h2>
         <nav>
@@ -76,6 +63,3 @@ return (
 };
 
 export default App;
-
-//以下メモ
-//
